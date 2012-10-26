13fa1f0b79ca7a3db86c9f42d23f2035092b622d
jesusmaherrera <jesusmaherrera@gmail.com>
2012-10-26 14:59:45 -0600

    Se agrego diseño para presentacion de noticias

--- index.php
+++ index.php
@@ -2,30 +2,142 @@
 	include("master_encabezado1.php");
 	include("master_menu1.php");
 	include("master_encabezado2.php");
+?>
+	<link rel="stylesheet" type="text/css" href="css/carousel/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/carousel/jquery.jscrollpane.css" media="all" />
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow&v1' rel='stylesheet' type='text/css' />
+	<link href='http://fonts.googleapis.com/css?family=Coustard:900' rel='stylesheet' type='text/css' />
+	<link href='http://fonts.googleapis.com/css?family=Rochester' rel='stylesheet' type='text/css' />
+<?php
 	include("master_contenido1.php");
 ?>
 <img src="images/pics01.png" width="1000" height="345" alt="" />
 <?php
 	include("master_contenido2.php");
 	include("master_segundocontenido1.php");
-
 ?>
-	<script type="text/javascript" src="swfobject.js"></script>
-	
+<script type="text/javascript" src="swfobject.js"></script>
 
-<div id="flashcontent" style="margin: -100px 0 0 -280px;">Para ver las imagenes requieres JavaScript y Flash Player. <a href="http://www.macromedia.com/go/getflashplayer/">Descargalos desde aquí</a> </div>	
-	<script type="text/javascript">
-		var fo = new SWFObject("autoviewer.swf", "autoviewer", "100%", "100%", "8", "#FFFFFF");		
-				
-		//Optional Configuration
-		//fo.addVariable("langOpenImage", "Open Image in New Window");
-		//fo.addVariable("langAbout", "About");	
-		//fo.addVariable("xmlURL", "gallery.xml");					
-		
-		fo.write("flashcontent");	
-		
-	</script>	
-	<?php
+
+<div class="container">
+			<h1>Noticias Recientes de <span>Hospital Médica Sierra</span></h1>
+			<div id="ca-container" class="ca-container">
+				<div class="ca-wrapper">
+
+					
+					<div class="ca-item ca-item-1">
+						<div class="ca-item-main">
+							<div class="ca-icon"></div>
+							<h3>Stop factory farming</h3>
+							<h4>
+								<span class="ca-quote">&ldquo;</span>
+								<span>The greatness of a nation and its moral progress can be judged by the way in which its animals are treated.</span>
+							</h4>
+								<a href="#" class="ca-more">more...</a>
+						</div>
+						<div class="ca-content-wrapper">
+							<div class="ca-content">
+								<h6>Animals are not commodities</h6>
+								<a href="#" class="ca-close">close</a>
+								<div class="ca-content-text">
+									<p>I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now.</p>
+									<p>When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream;</p>
+									<p>She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
+								</div>
+								<ul>
+									<li><a href="#">Read more</a></li>
+									<li><a href="#">Share this</a></li>
+									<li><a href="#">Become a member</a></li>
+									<li><a href="#">Donate</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="ca-item ca-item-2">
+						<div class="ca-item-main">
+							<div class="ca-icon"></div>
+							<h3>Respect Life &amp; Rights</h3>
+							<h4>
+								<span class="ca-quote">&ldquo;</span>
+								<span>I hold that the more helpless a creature, the more entitled it is to protection by man from the cruelty of man.</span>
+							</h4>
+								<a href="#" class="ca-more">more...</a>
+						</div>
+						<div class="ca-content-wrapper">
+							<div class="ca-content">
+								<h6>Would you eat your dog?</h6>
+								<a href="#" class="ca-close">close</a>
+								<div class="ca-content-text">
+									<p>I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now.</p>
+									<p>When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream;</p>
+									<p>She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
+								</div>
+								<ul>
+									<li><a href="#">Read more</a></li>
+									<li><a href="#">Share this</a></li>
+									<li><a href="#">Become a member</a></li>
+									<li><a href="#">Donate</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="ca-item ca-item-3">
+						<div class="ca-item-main">
+							<div class="ca-icon"></div>
+							<h3>Become 100% meat-free</h3>
+							<h4>
+								<span class="ca-quote">&ldquo;</span>
+								<span>I feel that spiritual progress does demand at some stage that we should cease to kill our fellow creatures for the satisfaction of our bodily wants.</span>
+							</h4>
+								<a href="#" class="ca-more">more...</a>
+						</div>
+						<div class="ca-content-wrapper">
+							<div class="ca-content">
+								<h6>You can change the world</h6>
+								<a href="#" class="ca-close">close</a>
+								<div class="ca-content-text">
+									<p>I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now.</p>
+									<p>When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream;</p>
+									<p>She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
+								</div>
+								<ul>
+									<li><a href="#">Read more</a></li>
+									<li><a href="#">Share this</a></li>
+									<li><a href="#">Become a member</a></li>
+									<li><a href="#">Donate</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<h5>Quotes by Mohandas Gandhi</h5>
+		</div>
+
+
+<script type="text/javascript" src="js/carousel/jquery.easing.1.3.js"></script>
+<!-- the jScrollPane script -->
+<script type="text/javascript" src="js/carousel/jquery.mousewheel.js"></script>
+<script type="text/javascript" src="js/carousel/jquery.contentcarousel.js"></script>
+<script type="text/javascript">
+	$('#ca-container').contentcarousel();
+</script>
+
+<div id="flashcontent" style="margin: -100px 0 0 -280px;">Para ver las imagenes requieres JavaScript y Flash Player. 
+	<a href="http://www.macromedia.com/go/getflashplayer/">Descargalos desde aquí</a> 
+</div>	
+<script type="text/javascript">
+	var fo = new SWFObject("autoviewer.swf", "autoviewer", "100%", "100%", "8", "#FFFFFF");		
+
+	//Optional Configuration
+	//fo.addVariable("langOpenImage", "Open Image in New Window");
+	//fo.addVariable("langAbout", "About");	
+	//fo.addVariable("xmlURL", "gallery.xml");					
+	
+	fo.write("flashcontent");	
+
+</script>	
+<?php
 	include("master_segundocontenido2.php");
 	include("master_piepagina.php");
 ?>
