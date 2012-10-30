1a67707c4b61f4e5d3904b85ef85aa34e9737aa6
jesusmaherrera <jesusmaherrera@gmail.com>
2012-10-30 10:50:23 -0700

    se actualizo imagenes desde servidor

--- master_db.php
+++ master_db.php
@@ -1,5 +1,5 @@
 <?PHP
-$connect=mysql_connect("paginahms.db.9623727.hostedresource.com","paginahms","Msierra12@")or die("no se conecto");
+$connect=mysql_connect("localhost","root","")or die("no se conecto");
 		mysql_select_db("paginahms"); 
-mysql_query("SET NAMES 'utf8'");
+
 ?>
