e575d6f29874a9869907371a837c9fcc64803a6a
jesusmaherrera <jesusmaherrera@gmail.com>
2012-10-30 17:05:04 -0700

    er

--- modificarimagennoticias1.php
+++ modificarimagennoticias1.php
@@ -20,30 +20,50 @@ $actual=$_POST['actual'];
   
   $partes=explode('.',$tmpname); 
   $extension=$partes[count($partes)-1]; 
-  $extension = strtolower($extension);
-  array_pop($partes); 
-  $tmpname=implode('',$partes).'.'.$extension; 
-  $name = $tmpname;
 
-  $tipo = $_FILES['imagen']['type'];
-  $tamano = $_FILES['imagen']['size'];
- 
-   include('/libraries/SimpleImage.php');
-   $image = new SimpleImage();
-   $image->load($tmpname);
-   $image->resize(300,226);
-   $image->output($directorio.$nombre);
-   move_uploaded_file($_FILES['imagen']['tmp_name'],$directorio.$nombre);
-   
+   $tipo = $_FILES["imagen"]['type'];
+
+
+  if ($tipo == "image/gif" || $tipo == "image/jpeg" || $tipo == "image/jpg")
+  {
+    array_pop($partes); 
+    $tmpname=implode('',$partes).'.'.$extension; 
+    $name = $tmpname;
+
+    $tipo = $_FILES['imagen']['type'];
+    $tamano = $_FILES['imagen']['size'];
+
+    include('/libraries/SimpleImage.php');
+    $image = new SimpleImage();
+    $image->load($tmpname);
+    $image->resize(300,226);
+    $image->save($directorio.$nombre);
+
+    $query="UPDATE noticias SET imagen='$nombre' , corta='BIEN' WHERE id=$uno";
+  }
+  else
+  {
+
+    if (move_uploaded_file($_FILES['imagen']['tmp_name'], $directorio.$nombre)) {
+        print "Received {$_FILES['imagen']['name']} - its size is {$_FILES['imagen']['size']}";
+    } else {
+        print "Upload failed!";
+    }
+    //move_uploaded_file($_FILES['imagen']['tmp_name'],$directorio.$nombre);
+    $query="UPDATE noticias SET imagen='$nombre', corta='MAL' WHERE id=$uno";
+  }
+
   include("master_db.php");
-	$query="UPDATE noticias SET imagen='$nombre' WHERE id=$uno";
+
+  
+    
 	$resultado=mysql_query($query) or die(mysql_error());
   
    unlink("images/noticias/".$actual);
 
 	?>
 	<script language="javascript"> 
-	document.location.href="modificarnoticias.php";
+	 document.location.href="modificarnoticias.php";
 	</script>
 </body>
 </html>
